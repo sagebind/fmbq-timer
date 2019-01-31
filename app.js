762f1d437226fa1b2f37d08f750687960b718e11
Stephen M. Coakley <me@stephencoakley.com>
2019-01-30 23:14:02 -0600

    Port UI to Vue, improve installation UI

--- app.js
+++ app.js
@@ -1,195 +1,171 @@
-var availableSounds = [
-    {
-        id: "bugaboo",
-        name: "Bugaboo",
-    },
-    {
-        id: "bumptious",
-        name: "Bumptious",
-    },
-    {
-        id: "correct",
-        name: "Correct",
-    },
-    {
-        id: "fanta",
-        name: "Fanta",
-    },
-    {
-        id: "foofaraw",
-        name: "Foofaraw",
-    },
-    {
-        id: "ludic",
-        name: "Ludic",
-    },
-    {
-        id: "tizzy",
-        name: "Tizzy",
-    },
-    {
-        id: "unctuous",
-        name: "Unctuous",
-    },
-]
+var audio = new Audio();
 
-var state = {
-    timerActive: false,
-    startTime: 0,
-    endTime: 0,
-    audio: new Audio(),
-}
+window.app = new Vue({
+    el: "main",
 
-function setCountdownValue(value) {
-    var text = (value / 1000).toFixed(1);
+    data: function () {
+        var settings = {
+            theme: "light",
+            timerSound: "tizzy",
+        };
 
-    if (app.countdownText.textContent != text) {
-        app.countdownText.textContent = text;
-    }
-}
-
-function setCountdownCircle(fraction) {
-    app.countdownCircle.setAttribute("stroke-dasharray", (90 * Math.PI * fraction) + " 1000");
-}
-
-function setTimerActive(active) {
-    state.timerActive = !!active;
-
-    if (active) {
-        document.body.classList.add("timer-active");
-    } else {
-        document.body.classList.remove("timer-active");
-    }
-}
-
-function playTimerSound() {
-    state.audio.src = "sounds/" + app.settings.timerSound + ".wav";
-    state.audio.currentTime = 0;
-    state.audio.play();
-}
-
-window.app = {
-    settings: {
-        theme: "light",
-        timerSound: "tizzy",
-    },
-
-    loadSettings: function () {
         var json = localStorage.getItem("settings");
         if (json) {
-            Object.assign(app.settings, JSON.parse(json));
+            Object.assign(settings, JSON.parse(json));
         }
+
+        return {
+            currentPage: "timer",
+            installed: false,
+            installPrompt: null,
+            interval: 0,
+            settings: settings,
+            timer: {
+                active: false,
+                startTime: 0,
+                endTime: 0,
+                remaining: 0,
+            },
+        };
     },
 
-    saveSettings: function () {
-        localStorage.setItem("settings", JSON.stringify(app.settings));
+    computed: {
+        timerText: function () {
+            return (this.timer.remaining / 1000).toFixed(1);
+        },
+
+        timerTotal: function () {
+            return this.timer.endTime - this.timer.startTime;
+        },
+
+        countdownCircleStyle: function () {
+            if (this.timer.active) {
+                var fraction = this.timer.remaining / this.timerTotal;
+                return (90 * Math.PI * fraction) + " 300";
+            }
+
+            return "300 300";
+        },
     },
 
-    setTheme: function (theme, callback) {
-        app.settings.theme = theme;
-        app.saveSettings();
+    watch: {
+        currentPage: function (name) {
+            document.querySelectorAll("main > section").forEach(function (page) {
+                if (page.id === name) {
+                    page.classList.add("active");
+                } else {
+                    page.classList.remove("active");
+                }
+            });
 
-        var link = document.getElementById("theme");
-        if (callback) {
-            link.addEventListener("load", callback, { once: true });
-        }
-        link.href = "themes/" + theme + ".css";
+            window.scrollTo(0, 0);
+        },
+
+        // Save settings automatically on change.
+        settings: {
+            deep: true,
+            handler: function (settings) {
+                localStorage.setItem("settings", JSON.stringify(settings));
+            },
+        },
+
+        "settings.theme": {
+            immediate: true,
+            handler: function (theme) {
+                document.getElementById("theme").href = "themes/" + theme + ".css";
+            },
+        },
+
+        "settings.timerSound": {
+            immediate: true,
+            handler: function (newValue, oldValue) {
+                audio.src = "sounds/" + newValue + ".wav";
+                if (oldValue) {
+                    this.playTimerSound();
+                }
+            },
+        },
     },
 
-    setTimerSound: function (name) {
-        if (app.settings.timerSound !== name) {
-            app.settings.timerSound = name;
-            playTimerSound();
-            app.saveSettings();
-        }
-    },
-
-    showPage: function (name) {
-        document.querySelectorAll("main > section").forEach(function (page) {
-            if (page.id === name) {
-                page.classList.add("active");
+    methods: {
+        install: function () {
+            if (this.installed) {
+                alert("App is already installed!");
+            } else if (!this.installPrompt) {
+                alert("Install is not supported on your device.");
             } else {
-                page.classList.remove("active");
+                (this.installPrompt)();
             }
-        });
+        },
 
-        window.scrollTo(0, 0);
+        startTimer: function (seconds) {
+            this.timer.startTime = performance.now();
+            this.timer.endTime = this.timer.startTime + (seconds * 1000);
+            this.timer.active = true;
+        },
+
+        cancelTimer: function () {
+            this.timer.active = false;
+            this.timer.remaining = 0;
+        },
+
+        playTimerSound: function () {
+            audio.currentTime = 0;
+            audio.play();
+        },
+
+        tick: function () {
+            if (this.timer.active) {
+                var remaining = Math.max(0, this.timer.endTime - performance.now());
+                this.timer.remaining = remaining;
+
+                if (remaining <= 0) {
+                    this.timer.active = false;
+                    this.playTimerSound();
+                }
+            }
+        },
+
+        showCurrentPage: function () {
+            if (location.hash.length > 1) {
+                this.currentPage = location.hash.substring(1);
+            } else {
+                this.currentPage = "timer";
+            }
+        },
     },
 
-    showCurrentPage: function () {
-        if (location.hash.length > 1) {
-            app.showPage(location.hash.substring(1));
-        } else {
-            app.showPage("timer");
+    created: function () {
+        // Set up offline cache handling.
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.register("service-worker.js");
         }
-    },
 
-    startTimer: function (seconds) {
-        state.startTime = performance.now();
-        state.endTime = state.startTime + (seconds * 1000);
-        setTimerActive(true);
-    },
+        this.showCurrentPage();
 
-    cancelTimer: function () {
-        setTimerActive(false);
-    },
-
-    install: function () {
         if (window.matchMedia("(display-mode: standalone)").matches) {
-            alert("App is already installed!");
-        } else {
-            alert("Install is not supported on your device.");
+            this.installed = true;
         }
+
+        this.interval = setInterval(this.tick, 50);
+
+        setTimeout(function () {
+            document.body.classList.add("ready");
+        }, 100);
     },
 
-    tick: function () {
-        if (state.timerActive) {
-            var total = state.endTime - state.startTime;
-            var remaining = Math.max(0, state.endTime - performance.now());
-
-            setCountdownValue(remaining);
-            setCountdownCircle(remaining / total);
-
-            if (remaining <= 0) {
-                setTimerActive(false);
-                playTimerSound();
-            }
-        } else {
-            setCountdownValue(0);
-            setCountdownCircle(1);
-        }
+    beforeDestroy: function () {
+        clearInterval(this.interval);
     },
-}
+});
 
 window.addEventListener("hashchange", app.showCurrentPage);
 
-window.addEventListener("beforeinstallprompt", function (promptEvent) {
-    promptEvent.preventDefault();
-    app.install = function () {
-        promptEvent.prompt();
-    };
+window.addEventListener("beforeinstallprompt", function (e) {
+    e.preventDefault();
+    app.installPrompt = e.prompt;
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Set up offline cache handling.
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("service-worker.js");
-    }
-
-    app.loadSettings();
-
-    app.countdownText = document.querySelector(".countdown-display .countdown-text");
-    app.countdownCircle = document.querySelector(".countdown-display .countdown-circle");
-
-    document.getElementById("setting-theme").value = app.settings.theme;
-    document.getElementById("setting-timer-sound").value = app.settings.timerSound;
-
-    app.setTheme(app.settings.theme);
-    app.showCurrentPage();
-
-    setInterval(app.tick, 50);
-
-    setTimeout(function () {
-        document.body.classList.add("ready");
-    }, 100);
+window.addEventListener("appinstalled", function () {
+    app.installed = true;
 });
