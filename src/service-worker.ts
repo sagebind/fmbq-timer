eb3284a7ceba00a87271ceddc1a3e360d7f6f49e
Stephen M. Coakley <me@stephencoakley.com>
2019-03-13 17:42:15 -0500

    Fix null bug in service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -24,13 +24,13 @@ self.addEventListener("install", (event: any) => {
 self.addEventListener("fetch", (event: any) => {
     event.respondWith((async () => {
         const cache = await caches.open("static");
+        let response = await cache.match(event.request);
 
-        try {
-            return await cache.match(event.request)
-        } catch (e) {
-            const response = await fetch(event.request);
+        if (!response) {
+            response = await fetch(event.request);
             cache.put(event.request, response.clone());
-            return response;
         }
+
+        return response;
     })());
 });
